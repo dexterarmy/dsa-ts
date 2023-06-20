07d2b60de05fb47da95b4a3e66efa92d3f98985b
dexterarmy <immohitsharma8@gmail.com>
2023-06-20 23:31:48 +0530

    recursion 2

--- recursion/recursion1.js
+++ recursion/recursion1.js
@@ -1,21 +1,24 @@
 'use strict';
 var arrOrg = [1, 2, 3, 4, 5, 6, 7];
-var target = 6;
+var array = [];
+var target = 1;
 // console.log(Math.ceil(arrOrg.length / 2));
 function binarySearch(arr) {
     if (arr[Math.floor(arr.length / 2)] === target)
         return arr[Math.floor(arr.length / 2)];
     if (arr[Math.floor(arr.length / 2)] > target) {
+        array = [];
         for (var i = 0; i < Math.floor(arr.length / 2); i++) {
-            arr = arr[i];
+            array.push(arr[i]);
         }
-        return binarySearch(arr);
+        return binarySearch(array);
     }
     if (arr[Math.floor(arr.length / 2)] < target) {
+        array = [];
         for (var i = Math.ceil(arr.length / 2); i < arr.length; i++) {
-            arr = arr[i];
+            array.push(arr[i]);
         }
-        return binarySearch(arr);
+        return binarySearch(array);
     }
     return 0;
 }
--- recursion/recursion1.ts
+++ recursion/recursion1.ts
@@ -1,26 +1,31 @@
 'use strict';
 
 const arrOrg:number[]  = [1,2,3,4,5,6, 7];
+let array : number[] = [];
 const target: number = 6;
 
 // console.log(Math.ceil(arrOrg.length / 2));
 
 function binarySearch(arr): number {
-    if (arr[Math.floor(arr.length / 2)] === target) return arr[Math.ceil(arr.length / 2)]; 
+    if (arr[Math.floor(arr.length / 2)] === target) return arr[Math.floor(arr.length / 2)]; 
     if (arr[Math.floor(arr.length / 2)] > target){
+        array = [];
         for(let i = 0 ; i < Math.floor(arr.length / 2); i++){
-            arr = arr[i];
+            
+            array.push(arr[i]);
         }
-        return binarySearch(arr);
+        return binarySearch(array);
 
     }
     if (arr[Math.floor(arr.length / 2)] < target){
+        array = [];
         for(let i = Math.ceil(arr.length / 2) ; i < arr.length; i++){
-            arr = arr [i];
+           
+            array.push(arr[i]);
         }
-        return binarySearch(arr);
+        return binarySearch(array);
     }
-    return 0 ;
+    return 0;
 }
 
 binarySearch(arrOrg);
